cf88989e7dbc6810802a4263fc2ab0b4f9350861
Tuba Bozkurt <tubzkrt@gmail.com>
2022-10-19 16:27:46 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const name = "Tugba";
+const Surname = "Bozkurt";
+const isLoggedIn = false;
+function App(){
+  return( <>
+  <h1>
+    {isLoggedIn ? `Benim adım ${name},soyadım ${Surname}`
+    :"Giriş Yapmadınız."}
+  </h1>
+  </>
+);
 }
-
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <div>Merhaba Ben Header Bileşeniyim.</div>
+        );
+    
+    
+}
+export default Header;
